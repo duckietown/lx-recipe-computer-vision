9b1a264f263c1a759feaefa0308f53ca751c2913
Liam Paull <paulll@iro.umontreal.ca>
2024-10-27 14:36:52 -0400

    initial commit

--- launchers/default.sh
+++ launchers/default.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 -m __solution__.main
+source /environment.sh
+
+source /opt/ros/noetic/setup.bash
+source /code/devel/setup.bash --extend
+
+exec roslaunch visual_lane_servoing visual_lane_servoing_node.launch veh:=$VEHICLE_NAME
